9a443440632ec8ff0234de370d3a8e3d4790a3d2
alvar <alvar@10.30.21.176>
2016-08-29 18:44:33 -0500

    Modified main method, and added a new method

--- src/NewClass.java
+++ src/NewClass.java
@@ -10,6 +10,9 @@
  */
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Java Class");
     }
+    public void method2(){
+        
+        }
 }
